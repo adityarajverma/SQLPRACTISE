f0d0db581acae1dc009168c422b4d0a13a3e1873
Aditya Raj Verma <adityarajverma@gmail.com>
2025-03-19 20:36:56 +0530

    Create 2_InsertDataQuery.sql

---
+++ 2_InsertDataQuery.sql
@@ -0,0 +1,53 @@
+INSERT INTO Student (STUDENT_ID, FIRST_NAME, LAST_NAME, GPA, ENROLLMENT_DATE, MAJOR) VALUES
+(1, 'John', 'Doe', 3.8, '2022-08-15', 'Computer Science'),
+(2, 'Jane', 'Smith', 3.6, '2021-07-10', 'Mechanical Engineering'),
+(3, 'Mike', 'Johnson', 3.2, '2023-01-05', 'Electrical Engineering'),
+(4, 'Emily', 'Davis', 3.9, '2022-06-20', 'Business Administration'),
+(5, 'David', 'Martinez', 3.5, '2020-09-12', 'Civil Engineering'),
+(6, 'Sophia', 'Garcia', 3.7, '2019-10-25', 'Mathematics'),
+(7, 'James', 'Brown', 3.3, '2021-03-14', 'Biology'),
+(8, 'Olivia', 'Miller', 3.4, '2023-02-11', 'Physics'),
+(9, 'Daniel', 'Wilson', 3.6, '2020-11-18', 'Chemistry'),
+(10, 'Isabella', 'Taylor', 3.8, '2019-12-09', 'Economics'),
+(11, 'Ethan', 'Anderson', 3.1, '2021-04-17', 'Political Science'),
+(12, 'Mia', 'Thomas', 3.5, '2022-05-22', 'Psychology'),
+(13, 'Alexander', 'Harris', 3.7, '2023-06-19', 'Sociology'),
+(14, 'Charlotte', 'Clark', 3.8, '2020-07-15', 'Philosophy'),
+(15, 'Benjamin', 'Lewis', 3.9, '2021-08-30', 'History');
+
+
+
+INSERT INTO Program (PROGRAM_ID, PROGRAM_NAME, DEPARTMENT_NAME) VALUES
+(1, 'Bachelor of Science in CS', 'Computer Science'),
+(2, 'Bachelor of Engineering in ME', 'Mechanical Engineering'),
+(3, 'Bachelor of Engineering in EE', 'Electrical Engineering'),
+(4, 'Bachelor of Business Administration', 'Business Administration'),
+(5, 'Bachelor of Engineering in CE', 'Civil Engineering'),
+(6, 'Bachelor of Science in Math', 'Mathematics'),
+(7, 'Bachelor of Science in Biology', 'Biology'),
+(8, 'Bachelor of Science in Physics', 'Physics'),
+(9, 'Bachelor of Science in Chemistry', 'Chemistry'),
+(10, 'Bachelor of Science in Economics', 'Economics'),
+(11, 'Bachelor of Arts in Political Science', 'Political Science'),
+(12, 'Bachelor of Arts in Psychology', 'Psychology'),
+(13, 'Bachelor of Arts in Sociology', 'Sociology'),
+(14, 'Bachelor of Arts in Philosophy', 'Philosophy'),
+(15, 'Bachelor of Arts in History', 'History');
+
+
+INSERT INTO Scholarship (SCHOLARSHIP_ID, STUDENT_REF_ID, SCHOLARSHIP_AMOUNT, SCHOLARSHIP_DATE) VALUES
+(1, 1, 5000.00, '2022-09-01'),
+(2, 2, 3000.00, '2021-08-15'),
+(3, 3, 4000.00, '2023-02-10'),
+(4, 4, 6000.00, '2022-07-20'),
+(5, 5, 2500.00, '2020-10-01'),
+(6, 6, 3500.00, '2019-11-25'),
+(7, 7, 4500.00, '2021-04-15'),
+(8, 8, 5000.00, '2023-03-01'),
+(9, 9, 3200.00, '2020-12-05'),
+(10, 10, 5500.00, '2019-11-20'),
+(11, 11, 2700.00, '2021-05-10'),
+(12, 12, 3300.00, '2022-06-15'),
+(13, 13, 4200.00, '2023-07-05'),
+(14, 14, 5300.00, '2020-08-30'),
+(15, 15, 6000.00, '2021-09-12');
